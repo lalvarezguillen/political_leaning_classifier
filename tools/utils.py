6c9f0f40622d82eecdec949625a7896fa3e5fb4d
Luis Alvarez <lalvarezguillen@gmail.com>
2016-06-17 23:09:19 +0000

    Started sketching

---
+++ tools/utils.py
@@ -0,0 +1,9 @@
+def removeUrls(document):
+    """Takes a string input, and removes the urls in it"""
+    # "http://[^ ]+" and "https://[^ ]+" seem like good regex patters to match urls
+    pass
+
+
+def storeStatement(statement, leaning, user):
+    """Stores a user statement in DB, with it's labeled political leaning"""
+    pass
