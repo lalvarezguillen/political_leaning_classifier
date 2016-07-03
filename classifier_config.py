2a445f01583a0358a6726cecbeb190b69a66647c
Luis Alvarez <lalvarezguillen@gmail.com>
2016-07-03 23:31:45 +0000

    Included the database. Tweaked the config code to handle relative DB path

--- classifier_config.py
+++ classifier_config.py
@@ -1,11 +1,16 @@
+import os
+
 #classfier to use. If none trains a new classifier
 pickled_classifier = ""
 
 
 #training dataset
-db_path = "/home/ubuntu/workspace/dataset.db"
+db_path = os.path.join(
+    os.path.dirname(__file__),
+    "dataset.db"
+)
 
 
 #Some network config
-app_port = 8080
+app_port = 80
 app_host = "0.0.0.0"
